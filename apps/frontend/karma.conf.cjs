7c226b0c2f1383eed5b3b5b481e602d07c8ea252
Illia Shatts <ilya.shatts@forvanagaming.com>
2026-04-14 12:28:55 +0200

    fix ci tests

---
+++ apps/frontend/karma.conf.cjs
@@ -0,0 +1,45 @@
+// Karma configuration for Angular unit tests.
+// Uses headless Chrome in CI (no display server).
+
+module.exports = function (config) {
+  const isCi = process.env.CI === 'true' || process.env.CI === '1';
+
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: !isCi,
+    browsers: [isCi ? 'ChromeHeadlessCI' : 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    singleRun: isCi,
+    restartOnFileChange: true,
+  });
+};
+
